5f4baf3fb600d320e2ecc5420c1489b0515fdc67
Elvis <elvis.omondi@student.moringaschool.com>
2024-07-21 16:17:29 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+// index.js
+
+// Function to return the first two drivers from an array
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  // Function to return the last two drivers from an array
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  // Array of functions to select different drivers
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // Function to create a fare multiplier
+  function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+  
+  // Function to double fares
+  const fareDoubler = createFareMultiplier(2);
+  
+  // Function to triple fares
+  const fareTripler = createFareMultiplier(3);
+  
+  // Function to select different drivers based on a function
+  function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+  }
